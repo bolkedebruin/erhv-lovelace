b6113a7121ed78ccf2f7ac9452332e4b5233cb8b
Silje Ljosland Bakke <silje.ljosland.bakke@helse-vest-ikt.no>
2023-10-20 20:24:33 +0200

    Replaced lit-import

--- swegon-casa-card.js
+++ swegon-casa-card.js
@@ -2,7 +2,7 @@ import {
   LitElement,
   html,
   css
-} from "https://unpkg.com/lit?module";
+} from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
 
 
 class SwegonCasaCard extends LitElement {
