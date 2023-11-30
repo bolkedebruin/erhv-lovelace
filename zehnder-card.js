9106dcf5e77739fe49fee8bab0d264c318658017
Bolke de Bruin <bolke@xs4all.nl>
2023-12-01 00:34:48 +0100

    Move

--- zehnder-card.js
+++ zehnder-card.js
@@ -35,24 +35,12 @@ class ZehnderCard extends LitElement {
     return html `
     <ha-card>
       <div class="container">
-        <svg version="1.1" viewBox="0 0 850 360" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <svg viewBox="0 0 850 360" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
           <defs>
             <linearGradient id="redblue">
               <stop stop-color="#acc0e5" offset="0" />
               <stop stop-color="#fd7c7a" offset="1" />
             </linearGradient>
-            <g id="fan" transform="scale(1.5 1.5)">
-              <path
-                  d="M12,11A1,1 0 0,0 11,12A1,1 0 0,0 12,13A1,1 0 0,0 13,12A1,1 0 0,0 12,11M12.5,2C17,2 17.11,5.57 14.75,6.75C13.76,7.24 13.32,8.29 13.13,9.22C13.61,9.42 14.03,9.73 14.35,10.13C18.05,8.13 22.03,8.92 22.03,12.5C22.03,17 18.46,17.1 17.28,14.73C16.78,13.74 15.72,13.3 14.79,13.11C14.59,13.59 14.28,14 13.88,14.34C15.87,18.03 15.08,22 11.5,22C7,22 6.91,18.42 9.27,17.24C10.25,16.75 10.69,15.71 10.89,14.79C10.4,14.59 9.97,14.27 9.65,13.87C5.96,15.85 2,15.07 2,11.5C2,7 5.56,6.89 6.74,9.26C7.24,10.25 8.29,10.68 9.22,10.87C9.41,10.39 9.73,9.97 10.14,9.65C8.15,5.96 8.94,2 12.5,2Z" />
-            </g>
-            <g id="speed" transform="scale(1.5 1.5)">
-              <path
-                  d="M12,16A3,3 0 0,1 9,13C9,11.88 9.61,10.9 10.5,10.39L20.21,4.77L14.68,14.35C14.18,15.33 13.17,16 12,16M12,3C13.81,3 15.5,3.5 16.97,4.32L14.87,5.53C14,5.19 13,5 12,5A8,8 0 0,0 4,13C4,15.21 4.89,17.21 6.34,18.65H6.35C6.74,19.04 6.74,19.67 6.35,20.06C5.96,20.45 5.32,20.45 4.93,20.07V20.07C3.12,18.26 2,15.76 2,13A10,10 0 0,1 12,3M22,13C22,15.76 20.88,18.26 19.07,20.07V20.07C18.68,20.45 18.05,20.45 17.66,20.06C17.27,19.67 17.27,19.04 17.66,18.65V18.65C19.11,17.2 20,15.21 20,13C20,12 19.81,11 19.46,10.1L20.67,8C21.5,9.5 22,11.18 22,13Z" />
-            </g>
-            <path id="arrow" d="m20 39.051h38.019v7.0848l14.228-14.228-14.228-14.228v7.0848h-38.019z" />
-            <g id="thermometer" transform="scale(2.4 2.4)">
-              <path d="M15 13V5A3 3 0 0 0 9 5V13A5 5 0 1 0 15 13M12 4A1 1 0 0 1 13 5V8H11V5A1 1 0 0 1 12 4Z" />
-            </g>
           </defs>
           <g transform="translate(5,15)">
             <g class="bg">
@@ -71,108 +59,80 @@ class ZehnderCard extends LitElement {
                 </g>
               </g>
             </g>
-            <g class="col-center" transform="translate(285,20)">
-              <foreignObject class="entity">
-                <div class="large_icon left">
-                  <svg>
-                      <use xlink:href='#thermometer' />
-                  </svg>
-                </div>
-                <div class="large_label left">
-                  ${this.hass.states[this.config.entity].attributes.temperature}°C
-                </div>
-              </foreignObject>
-            </g>
-            <g class="col-out">
-              <g transform="translate(0,-12)">
-                <foreignObject class="sensor">
-                  <div class="label left">
-                    <!-- outside temperature -->
-                    ${this.hass.states['sensor.ca350_outside_temperature'].state}°C
-                  </div>
-                </foreignObject>
-              </g>
-                <g transform="translate(0,90)">
-                  <foreignObject class="sensor">
-                    <div class="icon left">
-                      <!--<svg>
-                          <use xlink:href='#speed' />
-                      </svg>-->
-                      <ha-icon class="icon" icon="mdi:speedometer"></ha-icon>
-                    </div>
-                    <div class="label left">
-                      <!-- supply fan rpm -->
-                      ${Math.trunc(this.hass.states['sensor.ca350_supply_fan_speed'].state)} rpm
-                    </div>
-                  </foreignObject>
-                </g>
-                <g transform="translate(0,190)">
-                  <foreignObject class="sensor">
-                      <div class="label left">
-                        <!-- exhaust air temperature -->
-                        ${this.hass.states['sensor.ca350_exhaust_temperature'].state}°C
-                      </div>
-                  </foreignObject>
-                </g>
-                <g transform="translate(0,293)">
-                  <foreignObject class="sensor">
-                    <div class="icon left">
-                      <ha-icon class="icon" icon="mdi:speedometer"></ha-icon>
-                    </div>
-                    <div class="label left">
-                      <!-- exhaust fan speed -->
-                      ${Math.trunc(this.hass.states['sensor.ca350_exhaust_fan_speed'].state)} rpm
-                    </div>
-                  </foreignObject>
-                </g>
-            </g>
-            <g class="col-in" transform="translate(635,0)">
-              <g transform="translate(0,-12)">
-                <foreignObject class="sensor">
-                  <div class="label right">
-                    <!-- return temperature -->
-                    ${this.hass.states['sensor.ca350_return_temperature'].state}°C
-                  </div>
-                </foreignObject> 
-              </g>
-              <g transform="translate(0,90)">
-                <foreignObject class="sensor">
-                  <div class="label right">
-                    <!-- return air level in %-->
-                    ${Math.trunc(this.hass.states['sensor.ca350_return_air_level'].state)}%
-                  </div>
-                  <div class="medium_icon right">
-                    <svg>
-                      <use class="spin_extract" xlink:href='#fan' />
-                    </svg>
-                  </div>
-                </foreignObject>
-              </g>
-              <g transform="translate(0,190)">
-                <foreignObject class="sensor">
-                  <div class="label right">
-                    <!-- supply air temperature -->
-                    ${this.hass.states['sensor.ca350_supply_temperature'].state}°C
-                  </div>
-                </foreignObject>
-              </g>
-              <g transform="translate(0,293)">
-                <foreignObject class="sensor">
-                  <div class="label right">
-                    <!-- supply air level in %-->
-                    ${Math.trunc(this.hass.states['sensor.ca350_supply_air_level'].state)}%
-                  </div>
-                  <div class="medium_icon right">
-                    <svg>
-                      <use class="spin_supply" xlink:href='#fan' />
-                    </svg>
-                  </div>
-                </foreignObject>
-              </g>
-            </g>
-          </g>
         </svg>
       </div>
+      
+      <div id="col-out">
+        <div id="supply">
+          <div class="temperature" id="outside_temperature">
+              ${this.hass.states['sensor.ca350_outside_temperature'].state}°C
+          </div>
+          <div class="fan-speed" id="supply_fan_speed">
+              ${Math.trunc(this.hass.states['sensor.ca350_supply_fan_speed'].state)}
+          </div>
+        </div>
+        <div id="exhaust">
+          <div class="temperature">
+            <div class="label left">
+              <!-- exhaust air temperature -->
+              ${this.hass.states['sensor.ca350_exhaust_temperature'].state}°C
+            </div>
+          </div>  
+          <div class="fan-speed">
+            <div class="icon left">
+                <ha-icon class="icon" icon="mdi:speedometer"></ha-icon>
+            </div>
+            <div class="label left">
+                <!-- exhaust fan speed -->
+                ${Math.trunc(this.hass.states['sensor.ca350_exhaust_fan_speed'].state)} rpm
+            </div>
+          </div>  
+        </div>
+      <!-- end col-out -->
+      </div>
+
+      <div id="col-center">
+        <div class="large_icon left">
+          <ha-icon class="icon" icon="mdi:thermometer"></ha-icon>
+        </div>
+        <div class="large_label left">
+          ${this.hass.states[this.config.entity].attributes.temperature}°C
+        </div>
+      <!-- end col-center -->  
+      </div>
+      
+      <div id="col-in">
+        <div id="return">
+          <div class="label right">
+            <!-- return temperature -->
+            ${this.hass.states['sensor.ca350_return_temperature'].state}°C
+          </div>
+          <div class="label right">
+            <!-- return air level in %-->
+            ${Math.trunc(this.hass.states['sensor.ca350_return_air_level'].state)}%
+          </div>
+          <div class="icon right">
+            <ha-icon class="icon" icon="mdi:fan"></ha-icon>
+          </div>
+        </div>
+        <div id="supply_right">
+          <div class="label right">
+            <!-- supply air temperature -->
+            ${this.hass.states['sensor.ca350_supply_temperature'].state}°C
+          </div>
+          <div class="label right">
+            <!-- supply air level in %-->
+            ${Math.trunc(this.hass.states['sensor.ca350_supply_air_level'].state)}%
+          </div>
+          <div class="icon right">
+            <div class="icon right">
+              <ha-icon class="icon" icon="mdi:fan"></ha-icon>
+            </div>
+          </div>
+        </div>
+        <!-- end col-in -->
+      </div>
+      
       <div class="info-row">
       ${this.getAirFilterTmpl()}
       ${this.getPreHeatTmpl()}
